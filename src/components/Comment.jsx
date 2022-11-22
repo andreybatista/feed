007cf4c16f26ad8a4064badc2b579249cbb1b5cc
andreybatista <andreybatista131@gmail.com>
2022-11-22 18:38:04 -0300

    front end feed

---
+++ src/components/Comment.jsx
@@ -0,0 +1,47 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+import { Avatar } from './Avatar';
+
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/andreybatista.png"
+      />
+
+      <div className={styles.commentBox}>
+
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Andrey</strong>
+
+              <time
+                title='22 de Novembro às 17:10h'
+                dateTime='2022-11-22 17:08:00'
+              >
+                Cerca de 2h atrás
+              </time>
+            </div>
+
+            <button title='Deletar Comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
