73989b859aa787321fa812a1f921ddd05cc781d6
andreybatista <andreybatista131@gmail.com>
2022-11-26 17:04:13 -0300

    Deixando conteudos dinamicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
 import { Header } from "./components/Header"
 import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import styles from './App.module.css';
 
 import './global.css';
-import { Sidebar } from "./components/Sidebar";
 
+
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/andreybatista.png',
+      name: 'Andrey',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 github.com/andreybatista' },
+    ],
+    publishedAt: new Date('2022-11-22 17:08:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/andreybatista.png',
+      name: 'Andrey do Multiverso',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 github.com/andreybatista' },
+    ],
+    publishedAt: new Date('2022-11-10 17:08:00')
+  },
+];
 
 function App() {
   return (
@@ -15,8 +50,14 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </>
